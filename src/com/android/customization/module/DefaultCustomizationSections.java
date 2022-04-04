5f5efa575fb527910ec9aa41454a9d5a800c14a1
Anay Wadhera <anay1018@gmail.com>
2022-04-04 14:25:22 +0900

    add monet customization fragment
    Change-Id: I66bc08bf3aa897cda1456d04e7f3ac3643eaf1af
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.LifecycleOwner;
 import com.android.customization.model.font.FontManager;
 import com.android.customization.model.font.FontSectionController;
 import com.android.customization.model.color.ColorSectionController;
+import com.android.customization.model.color.MonetColorSectionController;
 import com.android.customization.model.grid.GridOptionsManager;
 import com.android.customization.model.grid.GridSectionController;
 import com.android.customization.model.iconpack.IconPackManager;
@@ -51,6 +52,10 @@ public final class DefaultCustomizationSections implements CustomizationSections
                 sectionControllers.add(new ColorSectionController(
                         activity, wallpaperColorsViewModel, lifecycleOwner, savedInstanceState,
                         sectionNavigationController));
+
+		// Monet colors
+	        sectionControllers.add(new MonetColorSectionController(sectionNavigationController));
+
 
                 // Dark/Light theme section.
                 sectionControllers.add(new DarkModeSectionController(activity,
